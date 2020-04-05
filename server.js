7db8d2cfa08e0bfa77793a6bcdeb17c49cfa320d
Leon Stevens <32592070+LeonStevens122@users.noreply.github.com>
2020-04-05 09:22:54 +0200

    Added front end using create-react-app

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-import express from 'express';
+const express = require( 'express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
 const helmet = require('helmet')
 const cors = require('cors');
 let path = require('path');
-
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config()
-
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
@@ -18,18 +16,12 @@ app.use(helmet())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-
-
-app.get('/', function(req, res) {
+app.get('/search/:query', function(req, res) {
   //  res.send('Hello World changed');
     axios.get('https://itunes.apple.com/search?term=jack+johnson')
         .then((response) => {
             res.send(response.data);
-            //console.log('Data : ', response.data);
-            //console.log(response.status);
-            //console.log(response.statusText);
-            //console.log(response.headers);
+           
             console.log('Config :  --------   -------', response.config);
         });
 
--- src/app (3).js
+++ src/app (3).js
@@ -36,5 +36,13 @@ app.get('/', function(req, res) {
 
 });
 
+axios.get('https://api.github.com/users/mapbox')
+    .then((response) => {
+        console.log(response.data);
+        console.log(response.status);
+        console.log(response.statusText);
+        console.log(response.headers);
+        console.log(response.config);
+    });
 
 app.listen(port);
